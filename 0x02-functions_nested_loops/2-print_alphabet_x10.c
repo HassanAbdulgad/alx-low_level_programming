3b74cf135ff7c0b8a6980b7a52324a71f399cbc1
HassanAbdulgad <hassanabbulgader16@gmail.com>
2023-09-13 17:52:06 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,6 @@
 void print_alphabet_x10(void)
 {
 	char ch;
-
 	int i;
 
 	i = 0;
@@ -20,7 +19,7 @@ void print_alphabet_x10(void)
 		    _putchar(ch);
 		    ch++;
 	    }
-	    _putchar('\n');
-	    i++;
+	     _putchar('\n');
+	     i++;
 	}
 }
